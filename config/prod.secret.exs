c5eb825d970c9fc55a92a5041207a2476413278a
Eiji MATSUMOTO <e.mattsan@gmail.com>
2019-10-22 10:52:27 +0900

    configure port with an environment variable

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,7 +12,7 @@ secret_key_base =
     """
 
 config :qr_code_server, QrCodeServerWeb.Endpoint,
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  http: [:inet6, port: {:system, "PORT"}],
   secret_key_base: secret_key_base
 
 # ## Using releases (Elixir v1.9+)
