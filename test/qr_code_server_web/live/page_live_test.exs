9918ece930f3c3cac59f1188b93c4d89f6344024
Eiji MATSUMOTO <e.mattsan@gmail.com>
2020-10-31 12:10:27 +0900

    fix tests

--- test/qr_code_server_web/live/page_live_test.exs
+++ test/qr_code_server_web/live/page_live_test.exs
@@ -5,7 +5,7 @@ defmodule QrCodeServerWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+    assert disconnected_html =~ "誤り訂正レベル"
+    assert render(page_live) =~ "誤り訂正レベル"
   end
 end
