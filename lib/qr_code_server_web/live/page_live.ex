18a53353bc568e916307d95cc89e3cae667e98d1
MATSUMOTO Eiji <e.mattsan@gmail.com>
2020-10-31 12:11:51 +0900

    Show qr code (#7)
    * show qr-code
    
    * can select levels
    
    * i18n
    
    * fix tests

--- lib/qr_code_server_web/live/page_live.ex
+++ lib/qr_code_server_web/live/page_live.ex
@@ -1,39 +1,94 @@
 defmodule QrCodeServerWeb.PageLive do
   use QrCodeServerWeb, :live_view
 
+  require Logger
+
+  @levels [:low, :medium, :quartile, :high]
+
+  @level_options [
+                   low: dgettext("error_correction_level", "low"),
+                   medium: dgettext("error_correction_level", "medium"),
+                   quartile: dgettext("error_correction_level", "quartile"),
+                   high: dgettext("error_correction_level", "high")
+                 ]
+                 |> Enum.map(fn {value, key} -> [value: value, key: key] end)
+
+  defguard is_level(level) when level in @levels
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
+    new_socket =
+      socket
+      |> assign(
+        qr_code: "",
+        text: "",
+        level: :low,
+        generating: false
+      )
+
+    {:ok, new_socket}
   end
 
   @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
+  def handle_event("submit", %{"source" => %{"text" => text}}, socket) do
+    send(self(), :update_qr_code)
+
+    new_socket =
+      socket
+      |> assign(
+        text: text,
+        generating: true
+      )
+
+    {:noreply, new_socket}
   end
 
   @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
+  def handle_event("change-level", %{"code" => %{"level" => level}}, socket) do
+    Logger.debug("change level: #{socket.assigns.level} -> #{level}")
 
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
+    send(self(), :update_qr_code)
+
+    new_socket =
+      socket
+      |> assign(
+        level: String.to_existing_atom(level),
+        generating: true
+      )
+
+    {:noreply, new_socket}
   end
 
-  defp search(query) do
-    if not QrCodeServerWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
+  @impl true
+  def handle_info(:update_qr_code, socket) do
+    settings = %QRCode.SvgSettings{scale: 5}
 
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
+    qr_code =
+      with %{text: text, level: level} when byte_size(text) > 0 and is_level(level) <-
+             Map.take(socket.assigns, [:text, :level]),
+           {:ok, qr} <- QRCode.create(text, level) do
+        {:safe, QRCode.Svg.create(qr, settings)}
+      else
+        %{text: text, level: level} ->
+          Logger.warning("Wrong parameter  text: #{inspect(text)} level: #{inspect(level)}")
+          ""
+
+        error ->
+          Logger.warning(inspect(error))
+          ""
+      end
+
+    new_socket =
+      socket
+      |> assign(
+        qr_code: qr_code,
+        generating: false
+      )
+
+    {:noreply, new_socket}
+  end
+
+  def level_options do
+    @level_options
   end
 end
