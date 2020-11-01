562f136a12e79b2e15adbd2e11e6200a83ef1794
Eiji MATSUMOTO <e.mattsan@gmail.com>
2020-11-01 13:15:58 +0900

    configure file name of qr-code files

--- lib/qr_code_server_web/controllers/download_controller.ex
+++ lib/qr_code_server_web/controllers/download_controller.ex
@@ -4,9 +4,20 @@ defmodule QrCodeServerWeb.DownloadController do
   require Logger
   alias QrCodeServer.Encoder
 
+  @timezone "Asia/Tokyo"
+
   def download(conn, %{"download" => download_params}) do
     Logger.debug("download params #{inspect(download_params)}")
-    qr_code = Encoder.encode(download_params["text"], String.to_existing_atom(download_params["level"]))
-    send_download(conn, {:binary, qr_code}, filename: "qr.svg") 
+
+    text = download_params["text"]
+    level = String.to_existing_atom(download_params["level"])
+    qr_code = Encoder.encode(text, level)
+
+    {:ok, filename} =
+      @timezone
+      |> Timex.now()
+      |> Timex.format("qr-code-%Y%m%d_%H%M%S.svg", :strftime)
+
+    send_download(conn, {:binary, qr_code}, filename: filename)
   end
 end
